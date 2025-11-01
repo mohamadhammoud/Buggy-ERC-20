c0dd8157ece3c53a966f257e7283cbd97ef60632
Mohamad Hammoud <mohamad.hammoudk@gmail.com>
2025-11-01 18:59:04 +0200

    ✅ assert Challenge14

--- test/Challenge14.t.sol
+++ test/Challenge14.t.sol
@@ -12,20 +12,28 @@ contract CounterTest is BaseTest {
     }
 
     function test_WrongAllowanceDecrementInTransferFrom() public {
-        uint256 amount = 0;
+        uint256 amount = 100 ether;
 
-        mockToken.approve(random, amount);
+        mockToken.mint(deployer, amount);
+
+        mockToken.approve(random, 1 ether);
 
         for (uint256 i; i < 100; i++) {
             vm.prank(random);
 
             // if (allowed == type(uint256).max) is incorrect
             // it should be if (allowed != type(uint256).max)
-            mockToken.transferFrom(deployer, random, amount);
+            mockToken.transferFrom(deployer, random, 1 ether);
         }
+
+        assertEq(amount, mockToken.balanceOf(random));
     }
 }
 
 contract MockToken is Challenge14 {
     constructor() Challenge14("Challenge14", "Ch14", 18) {}
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
 }
